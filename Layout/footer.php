f7b8ddd55eacca2c7e7d30e6780a83ff5319c26f
ali <ali.khosravani@gmail.coom>
2022-04-17 06:14:04 +0430

    Start store website

---
+++ Layout/footer.php
@@ -0,0 +1,7 @@
+
+<?php
+$copyYear = 2021;
+        $curYear = date('Y');
+        echo $copyYear . (($copyYear != $curYear) ? '-' . $curYear : '');
+        ?> کلیه حقوق برای پوشش کالا پویان و پارسا محفوظ است.
+        
